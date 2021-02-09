6b77249ee91c5656ce6861f22a2270ee129aa3c3
tikalestari <tika.dwilestari@gmail.com>
2021-02-08 22:23:46 -0600

    add clock in/out buttons

--- timesheetapp/timesheetapp/ViewControllers/HomeViewController.swift
+++ timesheetapp/timesheetapp/ViewControllers/HomeViewController.swift
@@ -9,21 +9,34 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var clockInButton: UIButton!
+    @IBOutlet weak var clockOutButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setUpElements()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setUpElements() {
+        // Filled rounded corner style
+        clockInButton.backgroundColor = UIColor.init(red: 52/255, green: 199/255, blue: 89/255, alpha: 1)
+        clockInButton.layer.cornerRadius = 25.0
+        clockInButton.tintColor = UIColor.white
+        
+        // Filled rounded corner style
+        clockOutButton.backgroundColor = UIColor.init(red: 255/255, green: 59/255, blue: 48/255, alpha: 1)
+        clockOutButton.layer.cornerRadius = 25.0
+        clockOutButton.tintColor = UIColor.white
     }
-    */
-
+    
+    @IBAction func clockInTapped(_ sender: Any) {
+        // TODO: enter clock-in time in db
+    }
+    
+    @IBAction func clockOutTapped(_ sender: Any) {
+        // TODO: enter clock-out time in db
+    }
+    
 }
